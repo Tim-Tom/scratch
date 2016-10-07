4ea25cb000e416f76caf3e8ee22244e5bd467070
Tim Bollman <Tim.Bollman@gmail.com>
2016-10-06 18:58:08 -0700

    Change Permutation function so it generates them all in a loop instead of returning control every time. I only ever get my result at the end of generating them all so I might as well do so.

--- table-sorting/src/TableSorting.adb
+++ table-sorting/src/TableSorting.adb
@@ -4,50 +4,16 @@ with Ada.Integer_Text_IO;
 procedure TableSorting is
    package IO renames Ada.Text_IO;
    package I_IO renames Ada.Integer_Text_IO;
-   
+
    Num_Elements : constant := 14;
    type Extended_Index is new Integer range 1 .. Num_Elements + 1;
    subtype Index is Extended_Index range 1 .. Num_Elements;
 
    a : Array(Index'Range) of Integer;
    output_width : constant := 3;
-   -- standard next Lexicographic order permutation function from knuth modified slightly
-   -- to ensure all indexes stay in bounds since I'm in Ada and it checks those things.
-   function Next_Permutation return Boolean is
-      i : Index := Index'Last;
-      j : Index := Index'Last;
-      temp : Integer;
-   begin
-      -- Find left pivot (first item that is less than a successor)
-      while i > Index'First and then a(i - 1) >= a(i) loop
-         i := i - 1;
-      end loop;
-      -- Dropped off the left side, no more pivots.
-      if i = Index'First then
-         return False;
-      end if;
-      -- Previously i was the head of the ascending suffix, change it to be the pivot.
-      i := i - 1;
-      -- Find the rightmost element that exceeds our pivot
-      while a(j) <= a(i) loop
-         j := j - 1;
-      end loop;
-      -- Swap the pivot and that element
-      temp := a(i);
-      a(i) := a(j);
-      a(j) := temp;
-      -- Reverse the suffix
-      i := i + 1;
-      j := Index'Last;
-      while i < j loop
-         temp := a(i);
-         a(i) := a(j);
-         a(j) := temp;
-         i := i + 1;
-         j := j - 1;
-      end loop;
-      return True;
-   end Next_Permutation;
+   
+   count : Natural := 0;
+
    procedure Print_Array is
    begin
       for i in Index'Range loop
@@ -59,19 +25,59 @@ procedure TableSorting is
          end if;
       end loop;
    end Print_Array;
+
+   -- Standard next Lexicographic order permutation function from Knuth modified slightly
+   -- to ensure all indexes stay in bounds since I'm in Ada and it checks those things.
+   procedure Permute is
+      i : Index;
+      j : Index;
+      temp : Integer;
+   begin
+      -- Output
+      count := count + 1;
+      loop
+         i := Index'Last;
+         j := Index'Last;
+         -- Find left pivot (first item that is less than a successor)
+         while i > Index'First and then a(i - 1) >= a(i) loop
+            i := i - 1;
+         end loop;
+         -- Dropped off the left side, no more pivots.
+         exit when i = Index'First;
+         -- Previously i was the head of the ascending suffix, change it to be the pivot.
+         i := i - 1;
+         -- Find the rightmost element that exceeds our pivot
+         while a(j) <= a(i) loop
+            j := j - 1;
+         end loop;
+         -- Swap the pivot and that element
+         temp := a(i);
+         a(i) := a(j);
+         a(j) := temp;
+         -- Reverse the suffix
+         i := i + 1;
+         j := Index'Last;
+         while i < j loop
+            temp := a(i);
+            a(i) := a(j);
+            a(j) := temp;
+            i := i + 1;
+            j := j - 1;
+         end loop;
+         -- Output
+         count := count + 1;
+      end loop;
+   end Permute;
+
    procedure Initialize(start : Extended_Index; size : Index) is
       next : Extended_Index := start;
-      count : Natural := 0;
    begin
       if size = 1 then
          for i in start .. Index'Last loop
             a(i) := 100 + Integer(start);
          end loop;
-         loop
-            count := count + 1;
-            -- Print_Array;
-            exit when not Next_Permutation;
-         end loop;
+         count := 0;
+         Permute;
          for i in Index'First .. Index'Last / 2 loop
             declare
                j : constant Index := Index'Last - i + 1;
@@ -94,7 +100,8 @@ procedure TableSorting is
          end loop;
       end if;
    end Initialize;
-   begin
+   
+begin
    Initialize(Index'First, 4);
    --  for i in Index'Range loop
    --     a(i) := Integer(i);
