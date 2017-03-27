0baf66750c39b73d028a0a81b28ae0cdb37c1b11
Tim Bollman <Tim.Bollman@gmail.com>
2017-03-27 16:59:15 -0700

    No longer allocate my own buffer.
    Okay, feels better now that I am not allocating my own buffers for writing stuff. It's
    still weird to me that they don't have single element writes available, but it's not
    really the worst thing ever to create a one element array to get around their interface.

--- random-art/art.rs
+++ random-art/art.rs
@@ -1,24 +1,21 @@
-use std::fs::File;
 use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufWriter;
 
 fn emit_greyscale(scale: i32) {
     let size: usize = 2*(scale as usize) + 1;
-    let mut output = File::create("test.pgm").unwrap();
+    let mut output = BufWriter::new(File::create("test.pgm").unwrap());
     // So apparently the size of arrays in rust have to be a compile time
     // constant at the moment. Hardcode the value because whatever.
-    let mut buf: [u8; 301] = [0; 301];
     write!(output, "P5 {} {} 255\n", size, size).unwrap();
     for xi in -scale .. (scale + 1) {
         for yi in -scale .. (scale + 1) {
             let x = xi as f32 / scale as f32;
             let y = yi as f32 / scale as f32;
             let intensity = (127.5 + 127.5*(x + y)/2.0) as u8;
-            if xi == 0 {
-                println!("Writing index {}", yi + scale);
-            }
-            buf[(yi + scale) as usize] = intensity;
+            let buf: [u8; 1] = [intensity];
+            output.write(&buf).unwrap();
         }
-        output.write(&buf).unwrap();
     }
 }
 fn main() {
