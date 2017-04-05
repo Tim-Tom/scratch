61e4c3e894a34536c7ccaf8124a945062e1b2ecb
Tim Bollman <Tim.Bollman@gmail.com>
2017-04-05 10:48:23 -0700

    General fixup.
    Add comments, normalize variable names.

--- HDT/HDT.pl
+++ HDT/HDT.pl
@@ -3,12 +3,7 @@ use warnings;
 
 my $filename = $ARGV[0] || 'HDT-data3.tsv';
 
-# I honestly don't know what the encoding here is, it looks like it's been re-encoded a
-# couple times, because it ends up as ??[symbol], which seems roundtripped through
-# something because there shouldn't be two question marks. We strip out any non-ascii
-# alphanum, so it doesn't particularly matter in the grand scheme of things.
-open(my $input, '<:encoding(latin1)', $filename) or die "Cannot open $filename for read: $!";
-
+# Set up a regular expression that will match any keyword in a title that we should ignore
 my $ignored = do {
   my @ignored = qw(a the for in and or is in are of);
   my $joined = join('|', map { quotemeta } @ignored);
@@ -17,27 +12,40 @@ my $ignored = do {
 
 my %keywords;
 my @articles;
-my $sum_log_views;
-# Skip header
-<$input>;
+my $sum_log_pageviews;
 
 sub update_keywords {
-  my ($kw1, $kw2, $type, $pageviews, $log_views, $article_id) = @_;
+  my ($kw1, $kw2, $type, $pageviews, $log_pageviews, $article_id) = @_;
   my $word = join("\t", $kw1, $kw2, $type);
-  my $keywords = ($keywords{$word} ||= { count => 0, pageviews => 0, log_pageviews => 0, min_pageviews => $log_views, max_pageviews => $log_views, occurances => [] });
+  my $keywords = ($keywords{$word} ||= { count => 0, pageviews => 0, log_pageviews => 0, min_pageviews => $log_pageviews, max_pageviews => $log_pageviews, occurances => [] });
   $keywords->{count}++;
   $keywords->{pageviews} += $pageviews;
-  $keywords->{log_pageviews} += $log_views;
-  $keywords->{max_pageviews} = $log_views if $log_views > $keywords->{max_pageviews};
-  $keywords->{min_pageviews} = $log_views if $log_views < $keywords->{min_pageviews};
+  $keywords->{log_pageviews} += $log_pageviews;
+  $keywords->{max_pageviews} = $log_pageviews if $log_pageviews > $keywords->{max_pageviews};
+  $keywords->{min_pageviews} = $log_pageviews if $log_pageviews < $keywords->{min_pageviews};
   push(@{$keywords->{occurances}}, $article_id);
   return $keywords;
 }
 
+##
+# -Part 1-
+# Read in the tab seperated data file, normalize it, and save off relevant information in
+# %keywords and @articles.
+##
+
+# I honestly don't know what the encoding here is, it looks like it's been re-encoded a
+# couple times, because it ends up as ??[symbol], which seems roundtripped through
+# something because there shouldn't be two question marks. We strip out any non-ascii
+# alphanum, so it doesn't particularly matter in the grand scheme of things.
+open(my $input, '<:encoding(latin1)', $filename) or die "Cannot open $filename for read: $!";
+
+# Skip header
+<$input>;
+
 while(<$input>) {
   chomp;
   my ($title, $url, $date, $pageviews) = split(/\t/, lc);
-  my $log_views = log(1 + $pageviews);
+  my $log_pageviews = log(1 + $pageviews);
   # Replace any non-alphanumeric characters with a space
   $title =~ s/[^a-z0-9]/ /g;
   # Remove words we ignore
@@ -48,21 +56,23 @@ while(<$input>) {
   $title =~ s/^ //;
   $title =~ s/ $//;
   my $type = ($url =~ /blogs/) ? 'BLOG' : 'OTHER';
-  push(@articles, [$., $title, $log_views, "BAD"]);
+  push(@articles, [$., $title, $log_pageviews, "BAD"]);
   my @keywords = split(/ +/, $title);
   for my $kw (@keywords) {
     my $word = "$kw\tN/A\t$type";
-    update_keywords($kw, 'N/A', $type, $pageviews, $log_views, $#articles);
+    update_keywords($kw, 'N/A', $type, $pageviews, $log_pageviews, $#articles);
   }
   for my $id (0 .. $#keywords - 1) {
-    update_keywords($keywords[$id], $keywords[$id+1], $type, $pageviews, $log_views, $#articles);
+    update_keywords($keywords[$id], $keywords[$id+1], $type, $pageviews, $log_pageviews, $#articles);
   }
-  $sum_log_views += $log_views;
+  $sum_log_pageviews += $log_pageviews;
 }
-
 close $input;
 
-my $average_views = $sum_log_views / @articles;
+##
+# - Part 2 -
+# Categorize articles as good or bad based on keyword pageviews.
+##
 
 open(my $output, '>:encoding(utf-8)', 'hdt-out.txt');
 open(my $reasons, '>:encoding(utf-8)', 'hdt-reasons.txt');
@@ -93,19 +103,24 @@ while (my ($word, $info) = each %keywords) {
 close $output;
 close $reasons;
 
+##
+# - Part 3 -
+# Validate the methodology used above and print out summary statistics.
+##
+
 my $threshold = 7.1;
-my ($good_articles, $bad_articles, $good_views, $bad_views, $real_good, $real_bad, $false_positive, $false_negative);
+my ($good_articles, $bad_articles, $good_pageviews, $bad_pageviews, $real_good, $real_bad, $false_positives, $false_negatives);
 
 for my $article (@articles) {
   my ($id, $title, $views, $status) = @$article;
   if ($status eq 'GOOD') {
     $good_articles++;
-    $good_views += $views;
-    $false_positive++ if $views < $threshold;
+    $good_pageviews += $views;
+    $false_positives++ if $views < $threshold;
   } else {
     $bad_articles++;
-    $bad_views += $views;
-    $false_negative++ if $views > $threshold;
+    $bad_pageviews += $views;
+    $false_negatives++ if $views > $threshold;
   }
   $real_good++ if $views > $threshold;
 }
@@ -114,18 +129,19 @@ $real_bad = @articles - $real_good;
 
 my $num_articles = @articles;
 my $num_keywords = keys %keywords;
-my $avg_good = $good_views / $good_articles;
-my $avg_bad = $bad_views / $bad_articles;
-my $error_rate = ($false_positive + $false_negative) / $num_articles;
+my $avg_pageviews = $sum_log_pageviews / @articles;
+my $avg_good = $good_pageviews / $good_articles;
+my $avg_bad = $bad_pageviews / $bad_articles;
+my $error_rate = ($false_positives + $false_negatives) / $num_articles;
 my $aggregation_factor = ($num_keywords / $good_keywords) / ($num_articles / $good_articles);
 
-print "Average pageviews: $average_views\n";
+print "Average pageviews: $avg_pageviews\n";
 print "Number of articles marked as good: $good_articles (real number is $real_good)\n";
 print "Number of articles marked as bad: $bad_articles (real number is $real_bad)\n";
 print "Avg pv: articles marked as good: $avg_good\n";
 print "Avg pv: articles marked as bad: $avg_bad\n";
-print "Number of false positive: $false_positive (bad marked as good)\n";
-print "Number of false negative: $false_negative (good marked as bad)\n";
+print "Number of false positives: $false_positives (bad marked as good)\n";
+print "Number of false negatives: $false_negatives (good marked as bad)\n";
 print "Number of articles: $num_articles\n";
 print "Error Rate: $error_rate\n";
 print "Number of feature values: $num_keywords (marked as good: $good_keywords)\n";
