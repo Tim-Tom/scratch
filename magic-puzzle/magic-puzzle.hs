7fd830cb9d6fc22374f610d915c1dc463e3ddc82
Tim Bollman <Tim.Bollman@gmail.com>
2017-04-20 11:00:11 -0700

    Have haskell do it's own printing.
    When testing I just used a simple perl script because it was simpler than writing this
    haskell when I didn't know if it was correct. I wanted to use printf like everything else,
    and haskell has it, but I would have had to install the Text module and I didn't feel like
    doing so for just this program.
    
    I'm also somewhat surprised mapi wasn't in the standard library. I know it is in F#.

--- magic-puzzle/magic-puzzle.hs
+++ magic-puzzle/magic-puzzle.hs
@@ -74,22 +74,37 @@ main =
     --     Validate (ind 16 [wm, wm+width .. size]), -- 16 = -> [3,7,11,15]
     --     Validate (ind 17 [wm, wm+wm .. size - width]), -- 17 -> [3,6,9,12]
     --     Solution]
-    --           0 1 2 3 4 5 6 7 8
-    locations = [0,1,2,6,3,5,7,8,4]
-    ind x lst = map (\y -> x - 1 - (locations !! y)) lst
+    
+    -- action_map is the action that assigns the given normal array index.
+    action_map = [0,1,2,6,3,5,7,8,4]
+    ind x lst = map (\y -> x - 1 - (action_map !! y)) lst
     actions = [
-      Choose,                    --  0 = 0
-      Choose,                    --  1 = 1
-      Decide   (ind 2 [0,1]),    --  2 = 2 -> [0 1]
-      Choose,                    --  3 = 4
-      Decide   (ind 4 [0,4]),    --  4 = 8 -> [0 4]
-      Decide   (ind 5 [2,8]),    --  5 = 5 -> [2 8]
-      Decide   (ind 6 [4,5]),    --  6 = 3 -> [4 5];
-      Decide   (ind 7 [0,3]),    --  7 = 6 -> [0 3];
-      Decide   (ind 8 [6,8]),    --  8 = 7 -> [6 8];
-      Validate (ind 9 [1,4,7]),  --  9 =   -> [1 4 7];
-      Validate (ind 9 [2,4,6]),  -- 10 =   -> [2 4 6];
+      Choose,                    --                  [0]
+      Choose,                    --                [1,0]
+      Decide   (ind 2 [0,1]),    --              [2,1,0]
+      Choose,                    --            [4,2,1,0]
+      Decide   (ind 4 [0,4]),    --          [8,4,2,1,0]
+      Decide   (ind 5 [2,8]),    --        [5,8,4,2,1,0]
+      Decide   (ind 6 [4,5]),    --      [3,5,8,4,2,1,0]
+      Decide   (ind 7 [0,3]),    --    [6,3,5,8,4,2,1,0]
+      Decide   (ind 8 [6,8]),    --  [7,6,3,5,8,4,2,1,0]
+      Validate (ind 9 [1,4,7]),  --  [7,6,3,5,8,4,2,1,0]
+      Validate (ind 9 [2,4,6]),  --  [7,6,3,5,8,4,2,1,0]
       Solution]
     (h:t) = actions
+    -- solution_map is the mapping from the picked index back to normal form
+    solution_map = [8,7,6,2,5,3,1,0,4]
     solutions = perform h t [] []
-  in do putStrLn (show solutions)
+    mapped_solutions = map (\solution -> map (\i -> solution !! i) solution_map) solutions
+    mapi :: (a -> Int -> b) -> [a] -> [b]
+    mapi f l = zipWith f l [1..]
+    chunked_print (a:b:c:rst) so_far =
+      let
+        df :: Int -> String
+        df d = let str = show d in if (length str) == 1 then " " ++ str else str
+        fmted = (df a) ++ " " ++ (df b) ++ " " ++ (df c) ++ "\n"
+        result = so_far ++ fmted
+      in case rst of
+        [] -> result
+        rst -> chunked_print rst result
+  in mapM_ putStr (mapi (\solution index -> chunked_print solution ("--- Solution " ++ (show index) ++ " ---\n")) mapped_solutions)
