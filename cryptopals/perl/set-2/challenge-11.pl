0f27173077f62193f4205e36450fcc21b0a65876
Tim Bollman <Tim.Bollman@gmail.com>
2017-08-18 09:29:50 -0700

    Challenge 11 does nothing so I'm not going to bother doing it.

---
+++ cryptopals/perl/set-2/challenge-11.pl
@@ -0,0 +1,8 @@
+
+print <<'END_MEH';
+
+Meh. This is below my interest level. Obviously I'm supposed to just check for repeated
+blocks like I did many exercises ago. Otherwise it's a bunch of busywork to create a
+function to randomly encode using the ciphers.
+
+END_MEH
