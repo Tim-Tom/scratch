582a8a8c74836e3aaee83b58dcd7bbf8919c59aa
Tim Bollman <Tim.Bollman@gmail.com>
2017-10-03 13:49:21 -0700

    Start playing with a new puzzle game.
    So far I've just turned the board into a graph. I had hoped the minimal spanning tree
    would have something to do with the solution, and while it does provide a lower bound it
    is trival to see in a 2x2 board of [rg][bg] that while the minimal spanning tree provides
    distances of 1, you would have to take 2 moves to solve the puzzle. Luckily lower bounds
    are all you need to have a valid A* search, so I can at least solve it heuristically.

---
+++ flood/README.md
@@ -0,0 +1,5 @@
+A simple truly free game on android. You are given a board, a number of colors, and you
+attempt to make the entire board one color by changing the color of the top left tile and
+all tiles connected to it horizontally and vertically of the given color. The game gives
+you a goal figure that I think can be achieved via something like a minimum spanning tree
+and I want to play with it to find out.
---
+++ flood/generate-graph.pl
@@ -0,0 +1,117 @@
+use v5.24;
+
+use strict;
+use warnings;
+
+use experimental 'signatures';
+
+use Term::ANSIColor qw(colored);
+
+my @nodes;
+my $root;
+
+my %nodes;
+
+my %colors = (
+  'R' => 'red',
+  'G' => 'green',
+  'B' => 'cyan'
+ );
+
+sub get_node($id) {
+  my $node = $id;
+  while (!ref $node) {
+    $node = $nodes{$node};
+  }
+  return $node;
+}
+
+sub make_node($color, $y, $x) {
+  state $id = 0;
+  my $node = { id => ++$id, color => $color, pos => [{x => $x, y => $y}], neighbors => [] };
+  $nodes{$node->{id}} = $node;
+  return $node->{id};
+}
+
+sub merge_nodes($n, $m) {
+  return ($n, $m) if ($n->{id} == $m->{id});
+  return merge_nodes($m, $n) if ($n->{id} > $m->{id});
+  die "invalid" if $n->{color} ne $m->{color};
+  $n->{pos} = [@{$n->{pos}}, @{$m->{pos}}];
+  # warn "Merging $m->{id} into $n->{id}" if ($n->{id} == 24 || $m->{id} == 24);
+  foreach my $neighbor (@{$m->{neighbors}}) {
+    my @neighbors = grep { $_->{id} != $m->{id} } @{ $neighbor->{neighbors} };
+    if (grep { $_->{id} == $n->{id} } @neighbors) {
+      $neighbor->{neighbors} = \@neighbors;
+    } else {
+      $neighbor->{neighbors} = [@neighbors, $n];
+      push(@{ $n->{neighbors} }, $neighbor);
+    }
+  }
+  # warn "Merged $n->{id} $m->{id}" if ($n->{id} == 24 || $m->{id} == 24);
+  $nodes{$m->{id}} = $n->{id};
+  return ($n, $n);
+}
+
+sub neighbor_nodes($n, $m) {
+  # warn "Neighbor $n->{id} $m->{id}" if ($n->{id} == 24 || $m->{id} == 24);
+  return merge_nodes($n, $m) if ($n->{color} eq $m->{color});
+  if (!grep { $_->{id} == $m->{id} } @{ $n->{neighbors} }) {
+    die if grep { $_->{id} == $n->{id} } @{ $m->{neighbors} };
+    push(@{$n->{neighbors}}, $m);
+    push(@{$m->{neighbors}}, $n);
+  }
+  return ($n, $m);
+}
+
+{
+  my @above;
+  my $first = 1;
+  my $line = 0;
+  while(<ARGV>) {
+    next if (/^#/);
+    chomp;
+    my $col = 0;
+    my @current = map { make_node($_, $line, $col++) } split(//);
+    if ($first) {
+      $root = $nodes{$current[0]};
+      for my $i (1 .. $#current) {
+        neighbor_nodes(map { get_node($_) } @current[$i-1, $i]);
+      }
+      $first = 0;
+    } else {
+      for my $i (0 .. $#current) {
+        neighbor_nodes(map { get_node($_) } $above[$i], $current[$i]);
+      }
+      for my $i (1 .. $#current) {
+        neighbor_nodes(map { get_node($_) } @current[$i-1, $i]);
+      }
+    }
+    @above = @current;
+    ++$line;
+  }
+  close ARGV;
+}
+
+# foreach my $id (sort { $a <=> $b } keys %nodes) {
+#   say "$id: $nodes{$id}->{id}";
+# }
+
+my @board;
+{
+  my %seen;
+  my @queue = $root;
+  while(my $node = shift @queue) {
+    next if $seen{$node->{id}}++;
+    push(@nodes, $node);
+    $node->{real_id} = @nodes;
+    foreach my $pos (@{$node->{'pos'}}) {
+      warn "$pos->{x} $pos->{y} $node->{id}: $board[$pos->{y}][$pos->{x}]->{id}" if $board[$pos->{y}][$pos->{x}];
+      $board[$pos->{y}][$pos->{x}] = $node;
+    }
+    push(@queue, @{$node->{neighbors}});
+  }
+  for my $row (@board) {
+    print join(' ', map { colored([$colors{$_->{color}}], sprintf('%3d', $_->{real_id})) } @$row)."\n";
+  }
+}
