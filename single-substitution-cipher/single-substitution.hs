62e90679dae75594d09d17cb8b97734144fe9f85
Tim Bollman <Tim.Bollman@gmail.com>
2017-11-08 10:35:01 -0800

    Add skeleton for solve.
    It handles everything except the guess case (where it currently just guesses the first
    letter without checking if it is right).

--- single-substitution-cipher/single-substitution.hs
+++ single-substitution-cipher/single-substitution.hs
@@ -6,9 +6,13 @@ import Text.Regex.PCRE
 data State = State {
   candidates :: [String],
   characters :: Map.Map Char [Char],
-  possible_words :: [[String]],
+  possibleWords :: [[String]],
   inclusion :: Map.Map Char [Bool]
 } deriving Show;
+
+getItem :: Ord k => k -> Map.Map k [v] -> [v]
+getItem k m =
+  Map.findWithDefault [] k m
 
 makePatternRec :: String -> [Char] -> Int -> String -> String
 makePatternRec [] _ _ p = reverse p
@@ -46,6 +50,27 @@ getPossibilities words patterns =
     emptySets = Map.fromList (map (\p -> (p, [])) patterns)
   in foldl (\ result (p,e) -> Map.adjust (\soFar -> e:soFar) p result) emptySets filtered
 
+solveRec :: State -> [Char] -> [Map.Map Char Char] -> [Map.Map Char Char]
+solveRec s [] solutions =
+  -- Lazy here. I should put better error messages in for debug
+  -- assert (all character lists have one element)
+  -- assert (all words have one possibilitiy)
+  (Map.map (\(e:[]) -> e) (characters s)) : solutions
+solveRec s (c:cs) solutions
+  | length (getItem c (characters s)) == 1 =
+    solveRec s cs solutions
+  | otherwise =
+    let
+      s2 = State {
+        candidates = (candidates s),
+        inclusion = (inclusion s),
+        possibleWords = (possibleWords s),
+        characters = (Map.adjust (\_ -> [c]) c (characters s))
+      } in solveRec s2 cs solutions
+
+solve :: State -> [Map.Map Char Char]
+solve  s = solveRec s (Map.keys (characters s)) []
+
 main :: IO ()
 main =
   do
@@ -60,7 +85,7 @@ main =
       state = State {
         candidates = words,
         characters = Map.fromList (map (\l -> (l, ['a' ..'z'])) letters),
-        possible_words = map (\w -> Map.findWithDefault [] (makePattern w) possible) words,
+        possibleWords = map (\w -> getItem (makePattern w) possible) words,
         inclusion = inclusion
       };
-    putStr (show inclusion)
+    putStr (show (solve state))
