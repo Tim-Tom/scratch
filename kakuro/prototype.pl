a4bddc7c9068d6ab82585a3412081ac9a70ecb9f
Tim Bollman <Tim.Bollman@gmail.com>
2017-11-23 09:40:51 -0800

    Add basic cell processing and constraining.
    The flow is a bit muddled at this point. There's a lot of special processing cases to
    imagine, so I'm going to mull for a bit before continuing.

--- kakuro/prototype.pl
+++ kakuro/prototype.pl
@@ -40,6 +40,7 @@ my @columns;
 my @rows;
 
 my @current_cols;
+my @cells;
 for my $row (0 .. ($vsize - 1)) {
   my @dots = split(//, read_line);
   die "Too few characters in board" unless @dots == $hsize;
@@ -49,19 +50,25 @@ for my $row (0 .. ($vsize - 1)) {
   for my $col (0 .. $#dots) {
     my $open = $dots[$col];
     if ($open) {
+      my $cell = bless({ data => [ 1 .. 9 ], row_index => $row, col_index => $col }, 'Cell');
       if ($current_cols[$col]) {
+        push(@{$current_cols[$col]->{cells}}, $cell);
         $current_cols[$col]->{end} = $row;
       } else {
-        my $data = { start => $row, end => $row };
+        my $data = bless({ start => $row, end => $row, cells => [$cell], dirty => 1 }, 'Constraint');
         push(@{$columns[$col]}, $data);
         $current_cols[$col] = $data;
       }
       if ($cur_row) {
+        push(@{$cur_row->{cells}}, $cell);
         $cur_row->{end} = $col;
       } else {
-        $cur_row = { start => $col, end => $col };
+        $cur_row = bless({ start => $col, end => $col, cells => [$cell], dirty => 1 }, 'Constraint');
         push(@{$rows[$row]}, $cur_row);
       }
+      $cell->{col} = $current_cols[$col];
+      $cell->{row} = $cur_row;
+      push(@cells, $cell);
     } else {
       $cur_row = $current_cols[$col] = undef;
     }
@@ -72,8 +79,12 @@ for my $row (0 .. $vsize - 1) {
   my @constraints = split(/\s+/, read_line);
   die "Number of constraints does not match board for row $row" if (@constraints != @{$rows[$row]});
   for my $i (0 .. $#constraints) {
-    $rows[$row][$i]->{constraint} = $constraints[$i];
-    $rows[$row][$i]->{length} = $rows[$row][$i]->{end} - $rows[$row][$i]->{start} + 1;
+    my $r = $rows[$row][$i];
+    $r->{constraint} = $constraints[$i];
+    $r->{length} = $rows[$row][$i]->{end} - $rows[$row][$i]->{start} + 1;
+    my $key = $r->{length} . '-' . $r->{constraint};
+    $r->{key} = $key;
+    $r->{possible} = [@{$possible_sums{$key}}];
   }
 }
 
@@ -81,7 +92,19 @@ for my $col (0 .. $hsize - 1) {
   my @constraints = split(/\s+/, read_line);
   die "Number of constraints does not match board for column $col" if (@constraints != @{$columns[$col]});
   for my $i (0 .. $#constraints) {
-    $columns[$col][$i]->{constraint} = $constraints[$i];
-    $columns[$col][$i]->{length} = $columns[$col][$i]->{end} - $columns[$col][$i]->{start} + 1;
+    my $c = $columns[$col][$i];
+    $c->{constraint} = $constraints[$i];
+    $c->{length} = $columns[$col][$i]->{end} - $columns[$col][$i]->{start} + 1;
+    my $key = $c->{length} . '-' . $c->{constraint};
+    $c->{key} = $key;
+    $c->{possible} = [@{$possible_sums{$key}}];
   }
 }
+
+for my $cell (@cells) {
+  my %rm = %{$mask{$cell->{row}{key}}};
+  my %cm = %{$mask{$cell->{col}{key}}};
+  $cell->{data} = [grep { $rm{$_} && $cm{$_} } @{$cell->{data}}];
+}
+
+p(@cells);
