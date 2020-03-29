aa2c9cf28309c54f68f8e94e1d377e5c988f5e29
Tim Bollman <tim.bollman@gmail.com>
2020-03-29 12:39:42 -0700

    Script to sort solutions.
    Allows me to diff the solutions for two implementations that generate in different orders.

---
+++ magic-puzzle/sort-solutions.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+
+use v5.24;
+
+for my $file (@ARGV) {
+  open(my $input, '<:encoding(ascii)', $file) or die;
+  my @solutions;
+  my $solution = '';
+  while(<$input>) {
+    if (/--- Solution \d+ ---/a) {
+      if ($solution) {
+        push(@solutions, $solution);
+      }
+      $solution = '';
+    } else {
+      $solution .= $_;
+    }
+  }
+  if ($solution) {
+    push(@solutions, $solution);
+  }
+  close $input;
+  open(my $output, '>:encoding(ascii)', $file) or die;
+  my $idx = 1;
+  for my $solution (sort @solutions) {
+    say $output "--- Solution $idx ---";
+    print $output $solution;
+    ++$idx;
+  }
+}
